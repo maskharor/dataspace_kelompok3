d5239b040ece1697866b2b6990052c5718670edb
maskharor <maskharor.amnan05@gmail.com>
2026-05-29 00:43:39 +0700

    restore routes and images from landingpage commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,46 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
-    return view('welcome');
+
+    if(Auth::check()){
+        return redirect()->route('dashboard');
+    }
+    return view('Users.landingpage');
+
+})->name('Users.landingpage');
+
+
+
+Route::middleware('guest')->group(function () {
+
+    Route::get('/login', [AuthController::class, 'login'])
+        ->name('login');
+
+    Route::post('/login', [AuthController::class, 'loginProses'])
+        ->name('login.proses');
+
+    Route::get('/register', [AuthController::class, 'register'])
+        ->name('register');
+
+    Route::post('/register', [AuthController::class, 'registerProses'])
+        ->name('register.proses');
+
+});
+
+
+Route::middleware('auth')->group(function () {
+
+    // DASHBOARD
+    Route::get('/dashboard', function () {
+        return view('dashboard.index');
+    })->name('dashboard');
+
+    // LOGOUT
+    Route::post('/logout', [AuthController::class, 'logout'])
+        ->name('logout');
+
 });
