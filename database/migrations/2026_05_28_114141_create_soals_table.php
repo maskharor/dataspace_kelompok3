972a68485e7d10eb64d060f6f7e0118b24cd10cb
maskharor <maskharor.amnan05@gmail.com>
2026-05-28 19:51:08 +0700

    add: migration DB

---
+++ database/migrations/2026_05_28_114141_create_soals_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('soals', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('modul_id')->constrained('moduls')->onDelete('cascade');
+            $table->text('pertanyaan');
+            $table->json('opsi_jawaban');
+            $table->string('kunci_jawaban'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('soals');
+    }
+};
