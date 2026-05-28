6a1655b65ec6f44dd1d1aba2bbe45f1508046e7c
maskharor <maskharor.amnan05@gmail.com>
2026-05-28 23:13:59 +0700

    modificate : routes for login & register pages

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,30 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/login', [AuthController::class, 'login'])
+    ->name('login');
+
+Route::post('/login', [AuthController::class, 'loginProses'])
+    ->name('login.proses');
+
+Route::get('/register', [AuthController::class, 'register'])
+    ->name('register');
+
+Route::post('/register', [AuthController::class, 'registerProses'])
+    ->name('register.proses');
+
+Route::post('/logout', [AuthController::class, 'logout'])
+    ->name('logout');
+
+Route::middleware(['auth'])->group(function () {
+
+    Route::get('/dashboard', function () {
+        return view('dashboard.index');
+    })->name('dashboard');
 });
